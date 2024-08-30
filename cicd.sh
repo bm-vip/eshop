ddd19a766f8da31ac6c82b11cc72795d543e3dde
behrooz <b.mohamadi@extratik.com>
2024-08-30 18:19:08 +0330

    add logger

--- cicd.sh
+++ cicd.sh
@@ -7,7 +7,7 @@ HOST="192.236.198.244"
 sudo ssh -T "root@$HOST" <<EOF
   cd /root/eshop
   git pull
-  sudo kill -9 $(sudo lsof -t -i:443)
+  sudo kill -9 $(sudo lsof -t -i:80)
   sudo kill -9 $(sudo lsof -t -i:2024)
   mvn clean package verify -DskipTests
   java -DPORT=2024 -DDB_URL=jdbc:postgresql://localhost:5432/eshop -jar /root/eshop/app/target/app-1.0.0-SNAPSHOT.jar &
