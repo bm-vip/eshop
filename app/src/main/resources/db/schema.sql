1e30b97e65e491b20c7ed006211faa2a558d2ef2
behrooz <b.mohamadi@extratik.com>
2024-09-01 21:46:20 +0330

    improve security

--- app/src/main/resources/db/schema.sql
+++ app/src/main/resources/db/schema.sql
@@ -1,3 +1,5 @@
-ALTER USER postgres PASSWORD '7nd2M487dMM7kLD4';
+-- ALTER USER postgres PASSWORD '7nd2M487dMM7kLD4';
+ALTER USER dml_user PASSWORD 'xmrtuKNbeE7O1kU';
 CREATE SCHEMA IF NOT EXISTS eshop;
-grant all privileges on database eshop to postgres;
+GRANT SELECT, INSERT, UPDATE, DELETE ON DATABASE eshop TO dml_user;
+-- grant all privileges on database eshop to postgres;
