c567b10f4bf9b65300ff46e2c2ff423018c609f6
behrooz <b.mohamadi@extratik.com>
2024-09-05 16:45:12 +0330

    add trading orders

--- client/src/main/resources/static/js/script/arbitrage.js
+++ client/src/main/resources/static/js/script/arbitrage.js
@@ -1,4 +1,5 @@
 $(function () {
+    //create subscription packages
     $.getJSON("/api/v1/subscription-package?size=100", function (subscriptionPackages) {
         $.get("/api/v1/subscription/find-active-by-user/" + currentUser.id, function (subscription) {
             subscriptionPackages.content.forEach(function (value) {
@@ -24,7 +25,7 @@ $(function () {
                                 </div>
                             </div>
                             <div class="pricing_footer">
-                                <a href="javascript:void(0);" class="btn btn-success btn-block ${!active ? 'disabled' : ''}" aria-disabled="${!active}" role="button">Trade now!</a> 
+                                <a href="javascript:trade();" class="btn btn-success btn-block ${!active ? 'disabled' : ''}" aria-disabled="${!active}" role="button">Trade now!</a> 
                                 <a href="javascript:void(0);" class="btn btn-primary btn-block ${active ? 'disabled' : ''}" aria-disabled="${active}" role="button">Purchase<span></span></a>                          
                             </div>
                         </div>
@@ -35,4 +36,65 @@ $(function () {
             });
         });
     });
+
 });
+function trade() {
+    $("#trading-content").show();
+    goToByScroll("#trading-content");
+    //create trading orders
+    $.get("/api/v1/subscription/find-active-by-user/" + currentUser.id, function (subscription) {
+        const orderCount = get(()=> subscription.subscriptionPackage.orderCount,0);
+        if(orderCount > 0) {
+            $("#trading-order").empty();
+            for (let i = 0; i < orderCount; i++) {
+                const orderElement = `              
+                <div class="col-md-3 widget widget_tally_box">
+                    <div class="x_panel">
+                        <div class="x_title">
+                            <h2>Order ${i+1}</h2>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="x_content">
+
+                            <div style="text-align: center; margin-bottom: 17px">
+                             <span class="chart" data-percent="100">
+                                  <span class="percent"></span>
+                             </span>
+                            </div>
+
+                            <h3 class="name_title">Finance</h3>
+                            <p>Short Description</p>
+
+                            <div class="divider"></div>
+
+                            <p>If you've decided to go in development mode and tweak all of this a bit, there
+                                are few things you should do.</p>
+                        </div>
+                    </div>
+                </div>`;
+                // Append the order element to the trading container
+                $("#trading-order").append(orderElement);
+            }
+            initPieChart();
+        }
+    });
+}
+function initPieChart(){
+    $('.chart').easyPieChart({
+        easing: 'easeOutElastic',
+        delay: 3000,
+        barColor: '#26B99A',
+        trackColor: '#fff',
+        scaleColor: false,
+        lineWidth: 20,
+        trackWidth: 16,
+        lineCap: 'butt',
+        onStep: function (from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
+        },
+        animate: {
+            duration: 5000, // 5 seconds animation duration
+            enabled: true
+        }
+    });
+}
