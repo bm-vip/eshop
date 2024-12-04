8165ee64d6a71195b598bfe6fc05810c1e9065d8
behrooz <b.mohamadi@extratik.com>
2024-12-04 16:34:59 +0330

    add region denied

--- client/src/main/java/com/eshop/client/config/HoneypotAuthenticationFilter.java
+++ client/src/main/java/com/eshop/client/config/HoneypotAuthenticationFilter.java
@@ -1,5 +1,8 @@
 package com.eshop.client.config;
 
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.model.CountryResponse;
+import lombok.SneakyThrows;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -8,31 +11,48 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.util.UUID;
 
 public class HoneypotAuthenticationFilter extends OncePerRequestFilter {
 
     private final String honeypotFieldName;
+    private DatabaseReader dbReader;
 
+    @SneakyThrows
     public HoneypotAuthenticationFilter(String honeypotFieldName) {
         this.honeypotFieldName = honeypotFieldName;
+        File database = new File("src/main/resources/GeoLite2-Country.mmdb");
+        dbReader = new DatabaseReader.Builder(database).build();
     }
 
+    @SneakyThrows
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
         MDC.put("traceId", UUID.randomUUID().toString());
-        String remoteAddr = "";
+        String ipAddress = "";
         if (request != null) {
-            remoteAddr = request.getHeader("X-FORWARDED-FOR");
-            if (StringUtils.isEmpty(remoteAddr)) {
-                remoteAddr = request.getRemoteAddr();
+            ipAddress = request.getHeader("X-FORWARDED-FOR");
+            if (StringUtils.isEmpty(ipAddress)) {
+                ipAddress = request.getRemoteAddr();
             }
         }
-        MDC.put("clientIp", remoteAddr);
+        InetAddress ip = InetAddress.getByName(ipAddress);
+        // Get country information
+        CountryResponse countryResponse = dbReader.country(ip);
+        String countryCode = countryResponse.getCountry().getIsoCode();
 
+        MDC.put("clientIp", ipAddress);
+
+        // Block specific countries
+        if ("CN".equals(countryCode) || "RU".equals(countryCode) || "IR".equals(countryCode)|| "KP".equals(countryCode)) {
+            response.sendRedirect("/region_denied.html");
+            return;
+        }
         if (isLoginRequest(request)) {
             String honeypotValue = request.getParameter(honeypotFieldName);
             if (honeypotValue != null && !honeypotValue.isEmpty()) {
